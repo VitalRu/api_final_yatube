a8c8c256be9fc26b89176415375ba72c72b1cdde
Виталий <thevitalru@gmail.com>
2023-05-17 17:40:06 +0700

    Add util method

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -49,10 +49,12 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly, )
 
+    def _get_post_object(self):
+        return get_object_or_404(Post, id=self.kwargs.get('post_id'))
+
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
         return post.comments.all()
 
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user, post=self._get_post_object())
